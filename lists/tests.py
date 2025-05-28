ed65a02eb62f9251da033a01c76fd721c4e3dc15
OPaimon <paimoe@bupt.edu.cn>
2025-05-29 01:38:28 +0800

    Add initial migrations and model for Item with text field; implement tests for saving and retrieving items

--- lists/tests.py
+++ lists/tests.py
@@ -4,12 +4,25 @@ from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
+from lists.models import Item
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second list item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, 'The first list item')
+        self.assertEqual(saved_items[1].text, 'The second list item')
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
