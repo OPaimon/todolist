2cb2e700f7672da5ab7d5daba8b729faba5e20d2
OPaimon <paimoe@bupt.edu.cn>
2025-06-01 23:17:22 +0800

    Add List model and associate items with lists; update views and tests accordingly

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -14,5 +14,6 @@ def new_list(request):
     if request.method == 'POST':
         new_item_text = request.POST.get('item_text', '')
         if new_item_text:
-            Item.objects.create(text=new_item_text)
+            list_ = List.objects.create()
+            Item.objects.create(text=new_item_text, list=list_)
         return redirect('/lists/the-new-page/')
